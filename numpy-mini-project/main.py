3a6fb0055f3ec54c48205c90c3bd44076f69c2af
vasava07 <sartikvasava@gmail.com>
2025-04-11 19:44:51 +0530

    Create main.py

---
+++ numpy-mini-project/main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+# Creating different types of arrays
+a = np.array([1, 2, 3])
+b = np.zeros((2, 2))
+c = np.ones((3, 1))
+d = np.arange(10)
+e = np.linspace(0, 1, 5)
+
+print("Array a:", a)
+print("Zeros:\n", b)
+print("Ones:\n", c)
+print("Range array:", d)
+print("Linspace array:", e)
