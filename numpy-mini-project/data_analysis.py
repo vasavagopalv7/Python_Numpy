47591e0fa8d8b8a7813460e6c8e5057f78c25131
vasava07 <sartikvasava@gmail.com>
2025-04-11 19:45:39 +0530

    Create data_analysis.py

---
+++ numpy-mini-project/data_analysis.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+data = np.array([10, 20, 20, 40, 50, 60, 70])
+
+print("Mean:", np.mean(data))
+print("Median:", np.median(data))
+print("Standard Deviation:", np.std(data))
+print("Minimum:", np.min(data))
+print("Maximum:", np.max(data))
